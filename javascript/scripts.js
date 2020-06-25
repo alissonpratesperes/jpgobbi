e4a2a53e91e18791f9d325cc2e078fe77a86a752
Alisson Prates Peres <a.pratesperes@gmail.com>
2020-06-24 22:45:46 -0300

    Implementação - Section - Serviços.
    Implementação totalmente refatorada do zero. A anterior estava cheia de bugs nos códigos, era codada somente em CSS, e não estava funcionando.
    Implementado um Slider com CSS e JS para as 5 sections de conteúdos que serão exibidas nessa Section de Serviços principal. Agora, é o JS que fica responsável pelas transferências das Sections e interações do usuário.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -6,6 +6,16 @@ const section_header = document.querySelector('section#header')
 
 let menuOpen = false;
 
+    const slider = document.querySelector('div.slider');
+
+    const leftArrow = document.querySelector('span.left');
+
+    const rightArrow = document.querySelector('span.right');
+
+    var indicatorParents = document.querySelector('ul#section_indicators');
+
+    var sectionIndex = 0;
+
 menu_btn.addEventListener('click', () => {
 
   if (!menuOpen) {
@@ -41,3 +51,60 @@ section_header.addEventListener('click', () => {
 	}
 
 });
+
+function setIndex() {
+
+    document.querySelector('div.controls ul#section_indicators .selected').classList.remove('selected');
+
+    slider.style.transform = 'translate(' + (sectionIndex) * -20 + '%)';
+
+}
+
+document.querySelectorAll('div.controls ul#section_indicators li').forEach(function(indicator, index){
+
+    indicator.addEventListener('click', function(){
+
+        sectionIndex = index;
+
+        setIndex();
+
+        indicator.classList.add('selected');
+
+    });
+
+});
+
+leftArrow.addEventListener('click', function(){
+
+    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+
+    setIndex();
+
+    indicatorParents.children[sectionIndex].classList.add('selected'); 
+
+});
+
+rightArrow.addEventListener('click', function() {
+
+    sectionIndex = (sectionIndex < 4) ? sectionIndex + 1 : 4;
+
+    setIndex();
+
+    indicatorParents.children[sectionIndex].classList.add('selected');
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
