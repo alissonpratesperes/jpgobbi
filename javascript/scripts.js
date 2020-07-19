7ac9387855fdd51d46a19a004dc66a2333d877ef
Alisson Prates Peres <a.pratesperes@gmail.com>
2020-07-19 17:10:38 -0300

    Implementação da Lógica do Carrossel.
    Implementada a Lógica JS do Carrossel na Section - Services.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -28,3 +28,87 @@ showMenu("nav-toggle", "nav-menu")
     }
 
     navLink.forEach(n => n.addEventListener("click", linkAction))
+
+
+
+
+
+const slider = document.querySelector('div.slider');
+
+const leftArrow = document.querySelector('span.left');
+
+const rightArrow = document.querySelector('span.right');
+
+var sectionIndex = 0;
+
+var indicatorParents = document.querySelector('ul#section_indicators');
+
+
+    function setIndex() {
+
+        document.querySelector('div.controls ul#section_indicators .selected').classList.remove('selected');
+
+        slider.style.transform = 'translate(' + (sectionIndex) * -20 + '%)';
+
+    }
+
+    document.querySelectorAll('div.controls ul#section_indicators li').forEach(function(indicator, index){
+
+        indicator.addEventListener('click', function(){
+
+            sectionIndex = index;
+
+            setIndex();
+
+            indicator.classList.add('selected');
+
+        });
+
+    });
+
+    leftArrow.addEventListener('click', function(){
+
+        sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+
+        setIndex();
+
+        indicatorParents.children[sectionIndex].classList.add('selected'); 
+
+    });
+
+    rightArrow.addEventListener('click', function() {
+
+        sectionIndex = (sectionIndex < 4) ? sectionIndex + 1 : 4;
+
+        setIndex();
+
+        indicatorParents.children[sectionIndex].classList.add('selected');
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
