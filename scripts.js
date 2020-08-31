a60536991fea3f96ba43a37af3b78604a67b431c
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2020-08-31 10:03:27 -0300

    Minificação final do CS da Section - Projects.
    Minificação final dos códigos-fonte do CSS (Inline CSS) da Section - Projects;
    Alteração de algumas classes novas (com padronização em Underscore_);
    Minificação final (Inline JS) dos Scripts JavaScript da página.

--- scripts.js
+++ scripts.js
@@ -6,23 +6,8 @@ const menu_item = document.querySelectorAll('section#header div.header div.nav_b
 
 const header = document.querySelector('section#header div.container');
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('active');
-            mobile_menu.classList.toggle('active');
-    });
+    hamburger.addEventListener('click', () => { hamburger.classList.toggle('active'); mobile_menu.classList.toggle('active'); });
 
-        document.addEventListener('scroll', () => {
-            var scroll_position = window.scrollY;
-                if(scroll_position > 50) {
-                    header.style.backgroundColor = '#29323C';
-                } else {
-                    header.style.backgroundColor = 'rgba(31, 30, 30, 0.24)';
-                }
-        });
+        document.addEventListener('scroll', () => { var scroll_position = window.scrollY; if(scroll_position > 50) { header.style.backgroundColor = '#29323C'; } else { header.style.backgroundColor = 'rgba(31, 30, 30, 0.24)'; } });
 
-            menu_item.forEach(item => {
-                item.addEventListener('click', () => {~
-                    hamburger.classList.toggle('active');
-                    mobile_menu.classList.toggle('active');
-                });
-            });
+            menu_item.forEach(item => { item.addEventListener('click', () => { hamburger.classList.toggle('active'); mobile_menu.classList.toggle('active'); }); });
