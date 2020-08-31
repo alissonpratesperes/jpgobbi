800752bfc2abd315ee3cd643e8d3bc927acc2894
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2020-08-31 09:05:56 -0300

    Minificação final do Main Header e JavaScript da página.
    Minificação dos códigos-fonte JavaScript e CSS do Main Header de Navegação do acesso Mobile.

--- scripts.js
+++ scripts.js
@@ -1,43 +1,28 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const hamburger = document.querySelector('section#header div.header div.nav_bar div.nav_list div.hamburger');
 
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const mobile_menu = document.querySelector('section#header div.header div.nav_bar div.nav_list ul');
 
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul a');
+const menu_item = document.querySelectorAll('section#header div.header div.nav_bar div.nav_list ul a');
 
-const header = document.querySelector('.header.container');
+const header = document.querySelector('section#header div.container');
 
     hamburger.addEventListener('click', () => {
-
         hamburger.classList.toggle('active');
-
             mobile_menu.classList.toggle('active');
-
     });
 
         document.addEventListener('scroll', () => {
-
             var scroll_position = window.scrollY;
-
                 if(scroll_position > 50) {
-
                     header.style.backgroundColor = '#29323C';
-
                 } else {
-
                     header.style.backgroundColor = 'rgba(31, 30, 30, 0.24)';
-
                 }
-
         });
 
             menu_item.forEach(item => {
-
                 item.addEventListener('click', () => {~
-                
                     hamburger.classList.toggle('active');
-
                     mobile_menu.classList.toggle('active');
-                    
                 });
-
             });
