661f64f6f26bbef37ee3d97b597cea44d8862118
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2020-09-01 17:13:23 -0300

    Refinamentos gerais de Interface.
    Realizados pequenos refinamentos gerais de Interface na Página.

--- scripts.js
+++ scripts.js
@@ -3,5 +3,5 @@ const mobile_menu = document.querySelector('section#header div.header div.nav_ba
 const menu_item = document.querySelectorAll('section#header div.header div.nav_bar div.nav_list ul a');
 const header = document.querySelector('section#header div.container');
     hamburger.addEventListener('click', () => { hamburger.classList.toggle('active'); mobile_menu.classList.toggle('active'); });
-        document.addEventListener('scroll', () => { var scroll_position = window.scrollY; if(scroll_position > 50) { header.style.backgroundColor = '#29323C'; } else { header.style.backgroundColor = 'rgba(31, 30, 30, 0.24)'; } });
+        document.addEventListener('scroll', () => { var scroll_position = window.scrollY; if(scroll_position > 50) { header.style.backgroundColor = '#29323C'; } else { header.style.backgroundColor = 'rgba(31, 30, 30, 0.24)'; } });    
             menu_item.forEach(item => { item.addEventListener('click', () => { hamburger.classList.toggle('active'); mobile_menu.classList.toggle('active'); }); });
