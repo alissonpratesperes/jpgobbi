f7e0081a15616f0e93404994563b070002a11e03
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2020-09-03 10:15:52 -0300

    ScrollSpy com Vanilla JS.
    Implementado o Scroll Spy com JavaScript puro nos três modos de acesso: Mobile, Tablet e Desktop.

--- scripts.js
+++ scripts.js
@@ -3,5 +3,11 @@ const mobile_menu = document.querySelector('section#header div.header div.nav_ba
 const menu_item = document.querySelectorAll('section#header div.header div.nav_bar div.nav_list ul a');
 const header = document.querySelector('section#header div.container');
     hamburger.addEventListener('click', () => { hamburger.classList.toggle('active'); mobile_menu.classList.toggle('active'); });
-        document.addEventListener('scroll', () => { var scroll_position = window.scrollY; if(scroll_position > 50) { header.style.backgroundColor = '#29323C'; } else { header.style.backgroundColor = 'rgba(31, 30, 30, 0.24)'; } });    
-            menu_item.forEach(item => { item.addEventListener('click', () => { hamburger.classList.toggle('active'); mobile_menu.classList.toggle('active'); }); });
+    document.addEventListener('scroll', () => { var scroll_position = window.scrollY; if(scroll_position > 50) { header.style.backgroundColor = '#29323C'; } else { header.style.backgroundColor = 'rgba(31, 30, 30, 0.24)'; } });    
+    menu_item.forEach(item => { item.addEventListener('click', () => { hamburger.classList.toggle('active'); mobile_menu.classList.toggle('active'); }); });
+        document.addEventListener('DOMContentLoaded', function(){
+            const sections = document.querySelectorAll('.content_section'); const menu_links = document.querySelectorAll('.nav_link a');
+            const makeActive = (link) => menu_links[link].classList.add('active'); const removeActive = (link) => menu_links[link].classList.remove('active'); const removeAllActive = () => [...Array(sections.length).keys()].forEach((link) => removeActive(link));
+            const sectionMargin = 200; let currentActive = 0;
+                window.addEventListener('scroll', () => { const current = sections.length - [...sections].reverse().findIndex((section) => window.scrollY >= section.offsetTop - sectionMargin) - 1
+                    if(current !== currentActive) { removeAllActive(); currentActive = current; makeActive(current); } }); }, false);
