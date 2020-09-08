7828e743ef42525bcf1111de93b0d1136a0c4ea5
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2020-09-08 11:14:55 -0300

    Minificação total do Scripts.JS.
    Minificação total realizada no arquivo de Scripts JavaScript;
    Sendo cada linha, um script;

--- scripts.js
+++ scripts.js
@@ -1,7 +1,4 @@
-const hamburger = document.querySelector('section#header div.header div.nav_bar div.nav_list div.hamburger');
-const mobile_menu = document.querySelector('section#header div.header div.nav_bar div.nav_list ul');
-const menu_item = document.querySelectorAll('section#header div.header div.nav_bar div.nav_list ul a');
-const header = document.querySelector('section#header div.container');
+const hamburger = document.querySelector('section#header div.header div.nav_bar div.nav_list div.hamburger'); const mobile_menu = document.querySelector('section#header div.header div.nav_bar div.nav_list ul'); const menu_item = document.querySelectorAll('section#header div.header div.nav_bar div.nav_list ul a'); const header = document.querySelector('section#header div.container');
 hamburger.addEventListener('click', () => { hamburger.classList.toggle('active'); mobile_menu.classList.toggle('active'); });
 document.addEventListener('scroll', () => { var scroll_position = window.scrollY; if(scroll_position > 50) { header.style.boxShadow = '0px 15px 25px rgba(0, 0, 0, 0.2)'; } else { header.style.boxShadow = 'none'; } });    
 menu_item.forEach(item => { item.addEventListener('click', () => { hamburger.classList.toggle('active'); mobile_menu.classList.toggle('active'); }); });
