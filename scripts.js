3846e6aedd0fc2bddf8fa23b65d8e5fd880d1857
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2020-09-11 19:00:34 -0300

    Tentativa de correção - Background Scroll no Menu.
    Código novo para a tentativa de correção do Background Scroll do Menu Mobile.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,33 @@
 const hamburger = document.querySelector('section#header div.header div.nav_bar div.nav_list div.hamburger'); const mobile_menu = document.querySelector('section#header div.header div.nav_bar div.nav_list ul'); const menu_item = document.querySelectorAll('section#header div.header div.nav_bar div.nav_list ul a'); const header = document.querySelector('section#header div.container');
 hamburger.addEventListener('click', () => { hamburger.classList.toggle('active'); mobile_menu.classList.toggle('active'); 
-function checkScroll() { if(hamburger.classList.contains('active')) { document.getElementsByTagName('html')[0].classList.add('dontScroll'); } else { document.getElementsByTagName('html')[0].classList.remove('dontScroll'); } } checkScroll(); });
+
+function checkScroll() { 
+
+
+    
+    if (hamburger.classList.contains('active')) { 
+
+        var InitScrollPosit = window.pageYOffset;
+        
+        document.getElementsByTagName('html')[0].classList.add('dontScroll');
+
+        console.log("MENU IS OPEN!", InitScrollPosit);
+    
+    } else {
+    
+        var currScrollPosit = window.pageYOffset;
+
+        console.log(currScrollPosit, "MENU IS CLOSED!");
+        
+        document.getElementsByTagName('html')[0].classList.remove('dontScroll');
+        document.getElementsByTagName('html')[0].scrollTo = currScrollPosit;
+    
+    }
+
+    currScrollPosit = InitScrollPosit;
+    
+    } checkScroll(); });
+
 menu_item.forEach(item => { item.addEventListener('click', () => { hamburger.classList.toggle('active'); document.getElementsByTagName('html')[0].classList.remove('dontScroll'); mobile_menu.classList.toggle('active'); }); });
 document.addEventListener('DOMContentLoaded', function(){ const sections = document.querySelectorAll('.content_section'); const menu_links = document.querySelectorAll('.nav_link a'); const makeActive = (link) => menu_links[link].classList.add('active'); const removeActive = (link) => menu_links[link].classList.remove('active'); const removeAllActive = () => [...Array(sections.length).keys()].forEach((link) => removeActive(link)); const sectionMargin = 200; let currentActive = 0; window.addEventListener('scroll', () => { const current = sections.length - [...sections].reverse().findIndex((section) => window.scrollY >= section.offsetTop - sectionMargin) - 1; if(current !== currentActive) { removeAllActive(); currentActive = current; makeActive(current); } }); }, false);
 function smoothScroll(target, duration) { var target = document.querySelector(target); var targetPosition = target.getBoundingClientRect().top; var startPosition = window.pageYOffset; var startTime = null; function animation(currentTime) { if(startTime === null) startTime = currentTime; var timeElapsed = currentTime - startTime; var run = ease(timeElapsed, startPosition, targetPosition, duration); window.scrollTo(0, run); if(timeElapsed < duration) requestAnimationFrame(animation); } function ease(t, b, c, d) { t /= d / 2; if (t < 1) return c / 2 * t * t + b; t--; return -c / 2 * (t * (t - 2) - 1) + b; } requestAnimationFrame(animation); } 
