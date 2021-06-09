a1e5f6b46e5e3ce59ffc71b016995e3d96767828
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-06-09 14:52:41 -0300

    Ajustes finais referente às implementações do Redesign do Site.
    Implementados e realizados os ajustes gerais referentes às implementações sobre o Redesign e "versão 2.0" do Site.

--- scripts.js
+++ scripts.js
@@ -1,179 +1,78 @@
 /* Declaration of Variables */
 const hamburger = document.querySelector('section#header div.header div.nav_bar div.nav_list div.hamburger');
-
 const mobile_menu = document.querySelector('section#header div.header div.nav_bar div.nav_list ul.nav_options');
-
 const menu_item = document.querySelectorAll('section#header div.header div.nav_bar div.nav_list ul.nav_options a');
-
 const header = document.querySelector('section#header div.header.container');
 /* End Declaration of Variables */
 
 /* CLICK WATCHER - Mobile Menu & Background Body Scrolling Block */
 hamburger.addEventListener('click', () => {
-    
     hamburger.classList.toggle('active');
-    
     mobile_menu.classList.toggle('active'); 
-
         function checkScroll() {
-            
-            if (hamburger.classList.contains('active')) {
-                
-                document.getElementsByTagName('html')[0].classList.add('dontScroll');
-            
-            } else { document.getElementsByTagName('html')[0].classList.remove('dontScroll');
-        
-            } 
-    
+            if (hamburger.classList.contains('active')) { document.getElementsByTagName('html')[0].classList.add('dontScroll'); }
+                else { document.getElementsByTagName('html')[0].classList.remove('dontScroll'); } 
         }
-        
             checkScroll();
-    
 });
 /* End CLICK WATCHER - Mobile Menu & Background Body Scrolling Block */
 
 /* CLICK WATCHER - Making  Menu Links 'active' through clicking or touching */
 menu_item.forEach(item => {
-    
     item.addEventListener('click', () => {
-        
         hamburger.classList.toggle('active');
-        
         document.getElementsByTagName('html')[0].classList.remove('dontScroll');
-        
         mobile_menu.classList.toggle('active');
-    
     });
-
 });
 /* End CLICK WATCHER - Making  Menu Links 'active' through clicking or touching */
 
 /* SCROLL WATCHER - Activating Menu Links when scrolling the page */
 document.addEventListener('DOMContentLoaded', function() {
-    
     const sections = document.querySelectorAll('.content_section');
-    
     const menu_links = document.querySelectorAll('.nav_link a');
-    
     const makeActive = (link) => menu_links[link].classList.add('active');
-    
     const removeActive = (link) => menu_links[link].classList.remove('active');
-    
     const removeAllActive = () => [...Array(sections.length).keys()].forEach((link) => removeActive(link));
-    
     const sectionMargin = 200;
-    
-    let currentActive = 0;
-    
-        window.addEventListener('scroll', () => {
-            
-            const current = sections.length - [...sections].reverse().findIndex((section) => window.scrollY >= section.offsetTop - sectionMargin) - 1;
-            
-                if(current !== currentActive) {
-                    
-                    removeAllActive();
-                    
-                    currentActive = current;
-                    
-                    makeActive(current);
-                
-                }
-        
-            });
-        
+        let currentActive = 0;
+            window.addEventListener('scroll', () => {
+                const current = sections.length - [...sections].reverse().findIndex((section) => window.scrollY >= section.offsetTop - sectionMargin) - 1;
+                    if(current !== currentActive) { removeAllActive(); currentActive = current; makeActive(current); }
+                });
 }, false);
 /* End SCROLL WATCHER - Activating Menu Links when scrolling the page */
 
 /* SCROLL WATCHER - Smooth Scrolling for 'crossbrowsing' effective script */
 function smoothScroll(target, duration) {
-    
     var target = document.querySelector(target);
-    
     var targetPosition = target.getBoundingClientRect().top;
-    
     var startPosition = window.pageYOffset;
-    
     var startTime = null;
-    
-    function animation(currentTime) {
-        
-        if(startTime === null) {
-            
-            startTime = currentTime
-        
-        };
-        
-        var timeElapsed = currentTime - startTime;
-        
-        var run = ease(timeElapsed, startPosition, targetPosition, duration);
-        
+        function animation(currentTime) {
+            if(startTime === null) { startTime = currentTime };
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, targetPosition, duration);
             window.scrollTo(0, run);
-            
-                if(timeElapsed < duration) {
-                    
-                    requestAnimationFrame(animation);
-                
-                };
-            
-    }
-    
-    function ease(t, b, c, d) { 
-        
-        t /= d / 2; if (t < 1) return c / 2 * t * t + b; t--; return -c / 2 * (t * (t - 2) - 1) + b; 
-    
+                if(timeElapsed < duration) { requestAnimationFrame(animation); }; 
+        }
+        function ease(t, b, c, d) { t /= d / 2; if (t < 1) return c / 2 * t * t + b; t--; return -c / 2 * (t * (t - 2) - 1) + b; } 
+            requestAnimationFrame(animation);
     } 
-    
-    requestAnimationFrame(animation);
 
-} 
-
-var scroll_button = document.querySelector('section#hero div.hero div.middle div.mouse');
-
-var emblem_link = document.querySelector('section#footer div.footer a#emblem_link');
-
-var hero_link = document.querySelector('section#header div.header div.nav_bar div.nav_list ul li.nav_link a#hero_link');
-
-var services_link = document.querySelector('section#header div.header div.nav_bar div.nav_list ul li.nav_link a#services_link');
-
-var projects_link = document.querySelector('section#header div.header div.nav_bar div.nav_list ul li.nav_link a#projects_link');
-
-var contact_link = document.querySelector('section#header div.header div.nav_bar div.nav_list ul li.nav_link a#contact_link');
+    var scroll_button = document.querySelector('section#hero div.hero div.middle div.mouse');
+    var emblem_link = document.querySelector('section#footer div.footer a#emblem_link');
+    var hero_link = document.querySelector('section#header div.header div.nav_bar div.nav_list ul li.nav_link a#hero_link');
+    var services_link = document.querySelector('section#header div.header div.nav_bar div.nav_list ul li.nav_link a#services_link');
+    var projects_link = document.querySelector('section#header div.header div.nav_bar div.nav_list ul li.nav_link a#projects_link');
+    var contact_link = document.querySelector('section#header div.header div.nav_bar div.nav_list ul li.nav_link a#contact_link');
     
-    scroll_button.addEventListener('click', function() {
-        
-        smoothScroll('section#services', 700);
-    
-    });
-    
-    emblem_link.addEventListener('click', function() {
-        
-        smoothScroll('section#hero', 700);
-    
-    });
-    
-    hero_link.addEventListener('click', function() {
-        
-        smoothScroll('section#hero', 700);
-    
-    });
-    
-    services_link.addEventListener('click', function() {
-        
-        smoothScroll('section#services', 700);
-    
-    });
-    
-    projects_link.addEventListener('click', function() {
-        
-        smoothScroll('section#projects', 700);
-    
-    });
-    
-    contact_link.addEventListener('click', function() {
-        
-        smoothScroll('section#contact', 700);
-    
-    });
+        scroll_button.addEventListener('click', function() { smoothScroll('section#services', 700); });
+        emblem_link.addEventListener('click', function() { smoothScroll('section#hero', 700); });
+        hero_link.addEventListener('click', function() { smoothScroll('section#hero', 700); });
+        services_link.addEventListener('click', function() { smoothScroll('section#services', 700); });
+        projects_link.addEventListener('click', function() { smoothScroll('section#projects', 700); });
+        contact_link.addEventListener('click', function() { smoothScroll('section#contact', 700); });
 /* End SCROLL WATCHER - Smooth Scrolling for 'crossbrowsing' effective script */
 
 /* SCROLL WATCHER - Autohide Navigation Bar when scrolling */
@@ -182,12 +81,9 @@ var last_scroll_position;
 var smartNavBarCall = document.querySelector('section#header');
     window.addEventListener('scroll', function(e) {
         last_scroll_position = window.scrollY;
-            if(new_scroll_position < last_scroll_position && last_scroll_position > 80) {  
-                smartNavBarCall.style.top = '-130px';
-            } else if ( new_scroll_position > last_scroll_position ) { 
-                smartNavBarCall.style.top = '0px';
-            }
-                new_scroll_position = last_scroll_position;
+            if(new_scroll_position < last_scroll_position && last_scroll_position > 80) { smartNavBarCall.style.top = '-150px'; }
+                else if ( new_scroll_position > last_scroll_position ) { smartNavBarCall.style.top = '0px'; }
+                    new_scroll_position = last_scroll_position;
 });
 /* End SCROLL WATCHER - Autohide Navigation Bar when scrolling */
 
